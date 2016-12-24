4b5ada9b218b5c356936fd2623c6e2bd208f9e90
Antoine Lafouasse <lafouaa@amazon.com>
2016-12-24 11:01:25 +0100

    Gradle skeleton

---
+++ src/main/java/fr/nate/slackbot/Application.java
@@ -0,0 +1,12 @@
+package fr.nate.slackbot;
+
+public class Application {
+
+    public static void main(String[] args) {
+        new Application().run();
+    }
+
+    public void run() {
+        System.out.println("hello");
+    }
+}
